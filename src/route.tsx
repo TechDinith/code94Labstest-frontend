8558e2cfc4edbeed3e58e3a2adbfa18a939d00a5
unknown <kaushikadinith1996@gmail.com>
2022-04-02 17:51:10 +0530

    almost done1

--- src/route.tsx
+++ src/route.tsx
@@ -1,4 +1,3 @@
-import { Layout } from "antd";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FavoritProductsPage from "./components/Pages/favoriteProductPage/favoriteProducts.page";
